ce032d88d0324810f1d642d91852afe5ee18ddc2
Tory <tory@Tory.local>
2016-12-13 22:15:25 +0800

    创建工程

---
+++ TRTabBarController/TRTabBarController/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TRTabBarController
+//
+//  Created by Tory on 2016/12/13.
+//  Copyright © 2016年 toryznoco. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TRTabBarController/TRTabBarController/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TRTabBarController
+//
+//  Created by Tory on 2016/12/13.
+//  Copyright © 2016年 toryznoco. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
